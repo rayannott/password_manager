c8fccdeae5f8f5f518f8f67c28f96e504d8d7fb3
rayannott <imjobjob@gmail.com>
2023-02-25 12:10:17 +0100

    no printing the hash

--- obj.py
+++ obj.py
@@ -279,7 +279,6 @@ class App:
                                 for ent in self.databases[db_name].entries:
                                     print(ent, file=fexport)
                         case ['info']:
-                            print(self.databases[db_name].key_hash)
                             if self.databases[db_name].get_unlocked():
                                 for msg in self.databases[db_name].display_info():
                                     print(msg)
